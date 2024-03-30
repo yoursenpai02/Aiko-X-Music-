ed0058836a60886162256d2d746439b5787d7fb7
yoursenpai02 <159107953+yoursenpai02@users.noreply.github.com>
2024-03-30 14:09:17 +0530

    Update start.py

--- DAXXMUSIC/plugins/bot/start.py
+++ DAXXMUSIC/plugins/bot/start.py
@@ -29,13 +29,13 @@ from strings import get_string
 #--------------------------
 
 NEXI_VID = [
-"https://telegra.ph/file/1a3c152717eb9d2e94dc2.mp4",
-"https://graph.org/file/ba7699c28dab379b518ca.mp4",
-"https://graph.org/file/83ebf52e8bbf138620de7.mp4",
-"https://graph.org/file/82fd67aa56eb1b299e08d.mp4",
-"https://graph.org/file/318eac81e3d4667edcb77.mp4",
-"https://graph.org/file/7c1aa59649fbf3ab422da.mp4",
-"https://graph.org/file/2a7f857f31b32766ac6fc.mp4",
+"https://telegra.ph/file/a581ebfaae0223f39bea5.jpg",
+"https://telegra.ph/file/d5c23cd534e8c05eb2096.jpg",
+"https://telegra.ph/file/dde4b85bc7b891b8c2188.jpg",
+"https://telegra.ph/file/d224c6dcc0949f47d7749.jpg",
+"https://telegra.ph/file/dc496b5091f737d6c07ed.jpg",
+"https://telegra.ph/file/a581ebfaae0223f39bea5.jpg",
+"https://telegra.ph/file/d5c23cd534e8c05eb2096.jpg",
 
 ]
 
